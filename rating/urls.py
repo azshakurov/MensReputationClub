1b45a02a84bf343f480ab3f2fce0ca379734f22e
GMCS\ashakurov <ashakurov@gmcs.ru>
2018-08-29 17:01:51 +0300

    Rating page, fix urls and model

--- rating/urls.py
+++ rating/urls.py
@@ -5,9 +5,9 @@ from rating.models import Rating
 
 
 urlpatterns = [
-    path(r'', ListView.as_view(
-        queryset=Rating.objects.all().order_by("-create_time")[:20],
-        template_name="rating/rating.html"
-    ), name='rating')
+    path(r'', RatingListView.as_view(
+            queryset=Rating.objects.all().order_by("-edit_time")[:20],
+            template_name="rating/rating.html"
+        ), name='rating')
 ]
 
--- rating/views.py
+++ rating/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from rating.models import Rating
 
 from MRC_site.urls import get_path_name
 
 
-def rating(request):
-    return render(request, 'rating/rating.html', context=get_path_name())
+class RatingListView(ListView):
+
+    model = Rating
+    paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_path_name())
+        return context
 
